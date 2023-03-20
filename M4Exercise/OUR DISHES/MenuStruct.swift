06ac18cba04825133d24bcb639acd945ed4b79a5
Mert Urhan <mert-urhan@hotmail.com>
2023-03-20 11:22:02 +0300

    Finished

---
+++ M4Exercise/OUR DISHES/MenuStruct.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+struct JSONMenu: Codable {
+    
+    let menu : [MenuItem]
+    
+    enum CodingKeys: String, CodingKey {
+        case menu = "menu"
+    }
+}
+
+
+struct MenuItem: Codable, Identifiable {
+    let id = UUID()
+    
+    let name : String
+    let price : String
+    
+    enum CodingKeys: String, CodingKey{
+        case name = "title"
+        case price = "price"
+    }
+}
